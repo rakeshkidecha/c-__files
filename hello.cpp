148476e0505bc887a6441696cec1001f00aad590
unknown <kidecharakesh2002@gmail.com>
2024-02-08 18:37:25 +0530

    first upload

---
+++ hello.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+// #define MAX 999;
+using namespace std;
+
+
+int main()
+
+{
+    // int a = 3;
+    
+    // float b = 6.4;
+
+    // bool c = true;
+
+
+    // cout << "velue of a.." << a << endl;
+
+    // cout << "velue of b.." << b << endl;
+
+    // cout << "velue of c.." << c <<endl;
+
+    // const float pi = 3.14;
+
+    // cout<< "pi velue..." <<pi;
+
+    // cout << MAX;
+
+    return 0;
+
+}
