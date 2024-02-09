09c4e7ae20f97401d8355b49d4d1b0355d532cb4
unknown <kidecharakesh2002@gmail.com>
2024-02-09 16:35:28 +0530

    static c++

---
+++ static.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+    // static program
+
+
+    int main()
+    {
+
+        int a=3; // declaration and initialization
+        int b=7;
+        int c;
+
+        cout << "a value is....." <<endl;
+        cout << a << endl;
+
+        cout << "b value is....." <<endl;
+        cout << b <<endl;
+
+        c=a+b;
+
+
+        cout << "a and b sum is..." <<endl ;
+        cout << c;
+
+
+
+    }
